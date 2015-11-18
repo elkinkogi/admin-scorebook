0418cd9f65d7a2bb3877e64ed5223ecc722234e6
Elkin Bernal <elkin@kogimobile.com>
2015-11-18 18:03:57 -0500

    commit two

--- app/scripts/controllers/listleague.js
+++ app/scripts/controllers/listleague.js
@@ -8,9 +8,82 @@
  * Controller of the adminAppApp
  */
 angular.module('adminAppApp')
-  .controller('ListleagueCtrl', function ($rootScope) {
+  .controller('ListleagueCtrl', function ($scope,SessionService, $location, $rootScope, $http) {
+   
+    if(!SessionService.get('isLogged')){
+        $location.path('/'); 
+    }
+    
+    
+     $rootScope.dataHeader = {
+         rightHeader:"Create New League",
+         pathRight:'listLeague',
+         leftHeader:"Home",
+         pathLeft:''
+     };
     
      $rootScope.header = true;
+    
+    var dataJSON = [
+            {
+                "league_name": "league name 2",
+                "number": "9",
+                "league_coor": "Elkin Bernal",
+                "game_played": "2"
+            },
+            {
+                "league_name": "league name 3",
+                "number": "9",
+                "league_coor": "Elkin Bernal",
+                "game_played": "2"
+            },
+            {
+                "league_name": "league name 4",
+                "number": "9",
+                "league_coor": "Elkin Bernal",
+                "game_played": "2"
+            },
+            {
+                "league_name": "league name 5",
+                "number": "9",
+                "league_coor": "Elkin Bernal",
+                "game_played": "2"
+            },
+            {
+                "league_name": "league name 6",
+                "number": "9",
+                "league_coor": "Elkin Bernal",
+                "game_played": "2"
+            },
+            {
+                "league_name": "league name 7",
+                "number": "9",
+                "league_coor": "Elkin Bernal",
+                "game_played": "2"
+            },
+            {
+                "league_name": "league name 8",
+                "number": "9",
+                "league_coor": "Elkin Bernal",
+                "game_played": "2"
+            },
+            {
+                "league_name": "league name 9",
+                "number": "9",
+                "league_coor": "Elkin Bernal",
+                "game_played": "2"
+            }
+    ];
+    $scope.listLeagues = dataJSON;
+    
+    console.log($scope.listLeagues);
+    
+    
+    $rootScope.logOut = function(){
+        alert('hola');
+         SessionService.unset('isLogged');
+        $location.path('/');
+    };
     
     
   });
--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -11,13 +11,20 @@ angular.module('adminAppApp')
   .controller('LoginCtrl', function ($scope, $rootScope, SessionService ,$http, $location) {
     
     var data_login = {};
-
+    
+    if(SessionService.get('isLogged')){
+               $location.path('/listLeague'); 
+            }
+    
     $scope.submitForm = function(){
         
         data_login.email    = $scope.email;
         data_login.password = $scope.password;
+        
+        
         if(data_login.email == "elkin-21@hotmail.com" && data_login.password == "password"){
             SessionService.set('isLogged', true, true);
+            
             $location.path('/listLeague');
         }
         
